3fff8ecb401264c10d3ab7b66a0628ea98dcaea3
Vishisht Gupta <vagupta1@myseneca.ca>
2022-04-21 20:58:30 -0400

    Final changes

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -1,22 +1,15 @@
 const { createSuccessResponse, createErrorResponse } = require('../../response');
 const { Fragment } = require('../../model/fragment');
-const contentType = require('content-type');
-const fs = require('fs');
 
 module.exports = async (req, res) => {
   try {
     const fragment = new Fragment({ ownerId: req.user, type: req.get('Content-Type') });
     await fragment.save();
-    const { type } = contentType.parse(req.get('Content-Type'));
-    if (type.startsWith('image/')) {
-      const buf = fs.readFileSync(req.body);
-      await fragment.setData(buf);
-    } else {
-      if (req.body == {} || req.body == '') {
-        throw new Error('Data not valid');
-      }
-      await fragment.setData(req.body);
+
+    if (req.body == {} || req.body == '') {
+      throw new Error('Data not valid');
     }
+    await fragment.setData(req.body);
 
     res.set('Location', ` http://${req.headers.host}/v1/fragments/${fragment.id}`);
     res.status(201).json(
